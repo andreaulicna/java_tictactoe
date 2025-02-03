fadcf0c95681412e0b10fa366caca2293417e8b5
Andrea Uličná <andrea@ulicna.com>
2025-02-03 22:14:56 +0100

    Fixes reading .env variables.

--- tictactoe_app/src/main/java/com/app/tictactoe/TicTacToeApplication.java
+++ tictactoe_app/src/main/java/com/app/tictactoe/TicTacToeApplication.java
@@ -3,20 +3,17 @@ package com.app.tictactoe;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.app.tictactoe")
 public class TicTacToeApplication {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		
-		Dotenv dotenv = Dotenv.load();
-	
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-	
-		SpringApplication.run(TicTacToeApplication.class, args);
-	}
+        // Load environment variables from .env
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        SpringApplication.run(TicTacToeApplication.class, args);
+    }
 }
